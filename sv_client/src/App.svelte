856db8aaf1ccf9c1bee94512eda794b01de67bfc
elton-cs <govada.elton@gmail.com>
2025-10-18 20:43:31 -0500

    fix: theme and color

--- sv_client/src/App.svelte
+++ sv_client/src/App.svelte
@@ -3,13 +3,13 @@
 </div>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap");
 
   :global(body) {
     background-color: black;
     color: white;
     margin: 0;
-    font-family: 'JetBrains Mono', monospace;
+    font-family: "JetBrains Mono", monospace;
   }
 
   .app-container {
