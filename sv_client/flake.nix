9c5a53e1d9ee2aa8e93c65d1323405d63780938c
elton-cs <govada.elton@gmail.com>
2025-10-18 19:04:26 -0500

    feat: add helix lsp with updated nix flake support

--- sv_client/flake.nix
+++ sv_client/flake.nix
@@ -15,12 +15,19 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
+            nodePackages.typescript-language-server
+            nodePackages.svelte-language-server
+            nodePackages.prettier
+            nodePackages.eslint
           ];
 
           shellHook = ''
             echo "sv_client dev env"
             echo "node version: $(node --version)"
             echo "npm version: $(npm --version)"
+            echo "typescript-language-server: $(typescript-language-server --version)"
+            echo "svelteserver: available"
+            echo "prettier: $(prettier --version)"
           '';
         };
       });
