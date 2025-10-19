d69a34319ae7a51e38beafd3d33092210deee007
elton-cs <govada.elton@gmail.com>
2025-10-18 19:52:43 -0500

    chore: add wasm to vite config

--- sv_client/vite.config.ts
+++ sv_client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), wasm(), topLevelAwait()],
 })
