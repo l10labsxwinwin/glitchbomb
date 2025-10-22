30f76cbc5c848feab4b2892d53e8967a8a64e541
elton-cs <govada.elton@gmail.com>
2025-10-21 21:02:14 -0500

    feat: add title

--- svk_recs_client/src/routes/+page.svelte
+++ svk_recs_client/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex items-center justify-center min-h-screen">
+	<h1 class="text-6xl font-bold">GLITCHBOMB</h1>
+</div>
