350f9c7531c8b6b9703757e395986b2103c2ec4c
elton-cs <govada.elton@gmail.com>
2025-10-21 20:14:58 -0500

    feat: create gamepacks subscription

--- svk_recs_client/src/lib/dojo/setup.svelte.ts
+++ svk_recs_client/src/lib/dojo/setup.svelte.ts
@@ -1,7 +1,12 @@
-import { init, type ParsedEntity, type SDK } from '@dojoengine/sdk';
+import { init, KeysClause, type ParsedEntity, type SDK } from '@dojoengine/sdk';
 import { ToriiQueryBuilder } from '@dojoengine/sdk';
 import { dojoConfig } from './dojoConfig';
-import { ModelsMapping, type Player, type SchemaType } from './typescript/models.gen';
+import {
+	ModelsMapping,
+	type Player,
+	type GamePack,
+	type SchemaType
+} from './typescript/models.gen';
 import type { Subscription } from '@dojoengine/torii-client';
 
 const dojoState = $state({
@@ -9,9 +14,11 @@ const dojoState = $state({
 	is_sdk_ready: false,
 
 	players: [] as Partial<Player>[],
+	gamepacks: [] as Partial<GamePack>[],
 
 	subscriptions: {
-		players: null as Subscription | null
+		players: null as Subscription | null,
+		gamepacks: null as Subscription | null
 	}
 });
 
@@ -88,5 +95,57 @@ export function cleanup_players_subscription() {
 	if (dojoState.subscriptions.players) {
 		dojoState.subscriptions.players.cancel();
 		dojoState.subscriptions.players = null;
+	}
+}
+
+export async function subscribe_to_gamepacks(player_id: string) {
+	if (!dojoState.sdk) {
+		throw new Error('SDK not initialized');
+	}
+
+	const [initialEntities, subscription] = await dojoState.sdk.subscribeEntityQuery({
+		query: new ToriiQueryBuilder()
+			.withEntityModels([ModelsMapping.GamePack])
+			.withClause(KeysClause([ModelsMapping.GamePack], [player_id], 'VariableLen').build())
+			.includeHashedKeys(),
+		callback: ({ data, error }) => {
+			if (data) {
+				data.forEach((entity) => {
+					const gamepack = entity.models.glitchbomb.GamePack;
+					if (gamepack) {
+						console.log('GamePack updated:', data);
+						const existingIndex = dojoState.gamepacks.findIndex(
+							(gp) => gp.player_id === gamepack.player_id && gp.gamepack_id === gamepack.gamepack_id
+						);
+						if (existingIndex !== -1) {
+							dojoState.gamepacks[existingIndex] = gamepack;
+						} else {
+							dojoState.gamepacks.push(gamepack);
+						}
+					}
+				});
+			}
+			if (error) {
+				console.error('Subscription error:', error);
+			}
+		}
+	});
+
+	console.log('initial gamepacks:');
+	initialEntities.getItems().forEach((entity: ParsedEntity<SchemaType>) => {
+		console.log(entity);
+		const gamepack = entity.models.glitchbomb.GamePack;
+		if (gamepack) {
+			dojoState.gamepacks.push(gamepack);
+		}
+	});
+
+	dojoState.subscriptions.gamepacks = subscription;
+}
+
+export function cleanup_gamepacks_subscription() {
+	if (dojoState.subscriptions.gamepacks) {
+		dojoState.subscriptions.gamepacks.cancel();
+		dojoState.subscriptions.gamepacks = null;
 	}
 }
