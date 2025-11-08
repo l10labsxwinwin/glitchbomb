d815d8bc6e0bc2ca940da8c83a89b9a3795c1ae4
elton-cs <govada.elton@gmail.com>
2025-11-07 19:56:36 -0600

    chore: space between layout

--- react_client/src/routes/free.tsx
+++ react_client/src/routes/free.tsx
@@ -24,7 +24,7 @@ function FreePlay() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-start bg-black text-white pt-12 md:pt-16 px-6 md:px-8">
-      <div className="flex flex-col items-center gap-8 md:gap-12 w-full max-w-7xl">
+      <div className="flex flex-col items-center justify-between w-full max-w-7xl flex-1 pb-12 md:pb-16">
         <div className="flex flex-row items-center gap-4 md:gap-8 justify-center">
           <CashOutButton onClick={() => console.log('Cash out clicked')} />
           <StatsDisplay gameData={mockGameData} />
