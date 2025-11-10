962192e76a6c31d86d8a7d9e0bf77620882b7417
elton-cs <govada.elton@gmail.com>
2025-11-10 05:45:52 -0600

    feat: display gamecontainer component on level route

--- react_client/src/routes/testnet/gamepack/$id/level.tsx
+++ react_client/src/routes/testnet/gamepack/$id/level.tsx
@@ -1,13 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import GameContainer from '@/components/GameContainer'
+import type { GameData } from '@/components/GameDataTypes'
 
 export const Route = createFileRoute('/testnet/gamepack/$id/level')({
   component: LevelStateRoute,
 })
 
 function LevelStateRoute() {
-  return (
-    <div className="flex justify-center items-center py-8">
-      <div className="text-white text-lg">Level in progress...</div>
-    </div>
-  )
+  // Default game data for now
+  const gameData: GameData = {
+    moonRocks: 0,
+    points: 0,
+    glitchChips: 0,
+    milestone: 0,
+    multiplier: 1,
+    bombs: 0,
+    health: 100,
+    pullable_orbs: [],
+    consumed_orbs: [],
+  }
+
+  return <GameContainer gameData={gameData} />
 }
